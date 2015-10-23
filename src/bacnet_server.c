c54027cecfb890fcdd17f8d109b6c69c96d9e7c9
Your Name <you@example.com>
2015-10-23 21:26:05 +1100

    Real final commit, compiles nicely!

--- src/bacnet_server.c
+++ src/bacnet_server.c
@@ -39,7 +39,7 @@ char *word;
 word_object *next;
 };
 
-static word_object *list_heads[NUM_LISTS];
+static word_object *list_head[listnum];
 
 static pthread_mutex_t list_lock = PTHREAD_MUTEX_INITIALIZER; static pthread_cond_t list_data_ready = PTHREAD_COND_INITIALIZER;
 static pthread_mutex_t timer_lock = PTHREAD_MUTEX_INITIALIZER;
@@ -72,214 +72,188 @@ pthread_mutex_unlock(&list_lock);
 pthread_cond_signal(&list_data_ready);
 }
 
-/*Initialise modbus structure*/
-static void *actmodbus(void *arg)
-{
-modbus_t *ctx;
-int rc;
-uint16_t tab_reg[4];
-int i;
-modstart:
-ctx = modbus_new_tcp(SERVER_ADDR, SERVER_PORT);
-if (ctx == NULL) {
-fprintf(stderr, "Unable to allocate lobmodbus context\n");
-sleep(1);
-goto modstart;
-
-}
-if (modbus_connect(ctx) == -1) {
-fprintf(stderr, "connection failed: %s\n", modbus_strerror(errno));
-sleep(1);
-modbus_free(ctx);
-goto modstart;
-
-} else {
-fprintf(stderr, "connection Successful\n");
-}
-
-/*Read registers*/
-while (1) {
-rc = modbus_read_registers(ctx, BACNET_INSTANCE_NO, NUM_INSTANCES,
-tab_reg);
-if (rc == -1) {
-fprintf(stderr, "%s\n", modbus_strerror(errno));
-//return -1;
-}
-
-	for (i = 0; i < rc; i++) {
-printf("reg[%d]=%d (0x%X)\n", i, tab_reg[i], tab_reg[i]);
-add_to_list(&list_heads[i], tab_reg[i]);
-}
-usleep(100000);
-}
-
-
-modbus_close(ctx);
-modbus_free(ctx);
-
-
-}
-
-
-
-
-
-#if 1
-/*This section is for testing procedure but still needs to be included*/
-
-static uint16_t test_data[] = {
-0xA4EC, 0x6E39, 0x8740, 0x1065, 0x9134, 0xFC8C
-};
-
-#define NUM_TEST_DATA (sizeof(test_data)/sizeof(test_data[0]))
-#endif
-
+//BACNET//
 static int Update_Analog_Input_Read_Property(
 BACNET_READ_PROPERTY_DATA *rpdata) {
-word_object *current_package; // variable
-uint16_t stack[3]; //second variable
+word_object *current_package; //placement variable
+uint16_t stack[3]; //second placement variable
 static int index;
 int instance_no = bacnet_Analog_Input_Instance_To_Index(
 rpdata->object_instance);
 if (rpdata->object_property != bacnet_PROP_PRESENT_VALUE) goto not_pv;
-pthread_mutex_lock(&list_lock); // lock list 
-
-if (list_heads[instance_no] != NULL) { current_object = list_get_first(&list_heads[instance_no]);
-
-bacnet_Analog_Input_Present_Value_Set(instance_no,
-current_object->number);
-printf("-------------------- %i:04X\n", instance_no,
-current_object->number);
-
-free(current_object);
+pthread_mutex_lock(&list_lock); // lock list
+/* Update the values to be sent to the BACnet client here.
+* The data should be read from the head of a linked list. You are required
+* to implement this list functionality.
+*
+* bacnet_Analog_Input_Present_Value_Set()
+* First argument: Instance No
+* Second argument: data to be sent
+*
+* Without reconfiguring libbacnet, a maximum of 4 values may be sent */
+// -------------------- EDIT HERE ====================================
+//
+//
+if (list_head[instance_no] == NULL) { //if no objects in list bail out
+pthread_mutex_unlock(&list_lock); // unlock list
+goto not_pv; //bail out, go to end of function
 }
-}
-
+current_package = list_get_first(&list_head[instance_no]); //update current package with data from first of list
+stack[instance_no] = strtol(current_package->word, NULL, 16); // convert string to long int.
+free(current_package); // release memory saved for current package
+pthread_mutex_unlock(&list_lock); // unlock list
+printf("AI_Present_Value request for instance %i\n", instance_no);
+bacnet_Analog_Input_Present_Value_Set(0, stack[instance_no]); // bacnet client gets data
+/* bacnet_Analog_Input_Present_Value_Set(1, test_data[index++]); */
+/* bacnet_Analog_Input_Present_Value_Set(2, test_data[index++]); */
+if (index == listnum) index = 0;
 not_pv:
-pthread_mutex_unlock(&list_lock);
 return bacnet_Analog_Input_Read_Property(rpdata);
 }
-
-
-
 static bacnet_object_functions_t server_objects[] = {
-    {bacnet_OBJECT_DEVICE,
-	    NULL,
-	    bacnet_Device_Count,
-	    bacnet_Device_Index_To_Instance,
-	    bacnet_Device_Valid_Object_Instance_Number,
-	    bacnet_Device_Object_Name,
-	    bacnet_Device_Read_Property_Local,
-	    bacnet_Device_Write_Property_Local,
-	    bacnet_Device_Property_Lists,
-	    bacnet_DeviceGetRRInfo,
-	    NULL, /* Iterator */
-	    NULL, /* Value_Lists */
-	    NULL, /* COV */
-	    NULL, /* COV Clear */
-	    NULL  /* Intrinsic Reporting */
-    },
-    {bacnet_OBJECT_ANALOG_INPUT,
-            bacnet_Analog_Input_Init,
-            bacnet_Analog_Input_Count,
-            bacnet_Analog_Input_Index_To_Instance,
-            bacnet_Analog_Input_Valid_Instance,
-            bacnet_Analog_Input_Object_Name,
-            Update_Analog_Input_Read_Property,
-            bacnet_Analog_Input_Write_Property,
-            bacnet_Analog_Input_Property_Lists,
-            NULL /* ReadRangeInfo */ ,
-            NULL /* Iterator */ ,
-            bacnet_Analog_Input_Encode_Value_List,
-            bacnet_Analog_Input_Change_Of_Value,
-            bacnet_Analog_Input_Change_Of_Value_Clear,
-            bacnet_Analog_Input_Intrinsic_Reporting},
-    {MAX_BACNET_OBJECT_TYPE}
+{bacnet_OBJECT_DEVICE,
+NULL,
+bacnet_Device_Count,
+bacnet_Device_Index_To_Instance,
+bacnet_Device_Valid_Object_Instance_Number,
+bacnet_Device_Object_Name,
+bacnet_Device_Read_Property_Local,
+bacnet_Device_Write_Property_Local,
+bacnet_Device_Property_Lists,
+bacnet_DeviceGetRRInfo,
+NULL, /* Iterator */
+NULL, /* Value_Lists */
+NULL, /* COV */
+NULL, /* COV Clear */
+NULL /* Intrinsic Reporting */
+},
+{bacnet_OBJECT_ANALOG_INPUT,
+bacnet_Analog_Input_Init,
+bacnet_Analog_Input_Count,
+bacnet_Analog_Input_Index_To_Instance,
+bacnet_Analog_Input_Valid_Instance,
+bacnet_Analog_Input_Object_Name,
+Update_Analog_Input_Read_Property,
+bacnet_Analog_Input_Write_Property,
+bacnet_Analog_Input_Property_Lists,
+NULL /* ReadRangeInfo */ ,
+NULL /* Iterator */ ,
+bacnet_Analog_Input_Encode_Value_List,
+bacnet_Analog_Input_Change_Of_Value,
+bacnet_Analog_Input_Change_Of_Value_Clear,
+bacnet_Analog_Input_Intrinsic_Reporting},
+{MAX_BACNET_OBJECT_TYPE}
 };
-
-static void modbus(void *arg) {
+static void register_with_bbmd(void) {
 #if RUN_AS_BBMD_CLIENT
-    /* Thread safety: Shares data with datalink_send_pdu */
-    bacnet_bvlc_register_with_bbmd(
-	    bacnet_bip_getaddrbyname(BACNET_BBMD_ADDRESS), 
-	    htons(BACNET_BBMD_PORT),
-	    BACNET_BBMD_TTL);
+/* Thread safety: Shares data with datalink_send_pdu */
+bacnet_bvlc_register_with_bbmd(
+bacnet_bip_getaddrbyname(BACNET_BBMD_ADDRESS),
+htons(BACNET_BBMD_PORT),
+BACNET_BBMD_TTL);
 #endif
 }
-
 static void *minute_tick(void *arg) {
-    while (1) {
-	pthread_mutex_lock(&timer_lock);
-
-	/* Expire addresses once the TTL has expired */
-	bacnet_address_cache_timer(60);
-
-	/* Re-register with BBMD once BBMD TTL has expired */
-	register_with_bbmd();
-
-	/* Update addresses for notification class recipient list 
-	 * Requred for INTRINSIC_REPORTING
-	 * bacnet_Notification_Class_find_recipient(); */
-	
-	/* Sleep for 1 minute */
-	pthread_mutex_unlock(&timer_lock);
-	sleep(60);
-    }
-    return arg;
+while (1) {
+pthread_mutex_lock(&timer_lock);
+/* Expire addresses once the TTL has expired */
+bacnet_address_cache_timer(60);
+/* Re-register with BBMD once BBMD TTL has expired */
+register_with_bbmd();
+/* Update addresses for notification class recipient list
+* Requred for INTRINSIC_REPORTING
+* bacnet_Notification_Class_find_recipient(); */
+/* Sleep for 1 minute */
+pthread_mutex_unlock(&timer_lock);
+sleep(60);
 }
-
+return arg;
+}
 static void *second_tick(void *arg) {
-    while (1) {
-	pthread_mutex_lock(&timer_lock);
-
-	/* Invalidates stale BBMD foreign device table entries */
-	bacnet_bvlc_maintenance_timer(1);
-
-	/* Transaction state machine: Responsible for retransmissions and ack
-	 * checking for confirmed services */
-	bacnet_tsm_timer_milliseconds(1000);
-
-	/* Re-enables communications after DCC_Time_Duration_Seconds
-	 * Required for SERVICE_CONFIRMED_DEVICE_COMMUNICATION_CONTROL
-	 * bacnet_dcc_timer_seconds(1); */
-
-	/* State machine for load control object
-	 * Required for OBJECT_LOAD_CONTROL
-	 * bacnet_Load_Control_State_Machine_Handler(); */
-
-	/* Expires any COV subscribers that have finite lifetimes
-	 * Required for SERVICE_CONFIRMED_SUBSCRIBE_COV
-	 * bacnet_handler_cov_timer_seconds(1); */
-
-	/* Monitor Trend Log uLogIntervals and fetch properties
-	 * Required for OBJECT_TRENDLOG
-	 * bacnet_trend_log_timer(1); */
-	
-	/* Run [Object_Type]_Intrinsic_Reporting() for all objects in device
-	 * Required for INTRINSIC_REPORTING
-	 * bacnet_Device_local_reporting(); */
-	
-	/* Sleep for 1 second */
-	pthread_mutex_unlock(&timer_lock);
-	sleep(1);
-    }
-    return arg;
+while (1) {
+pthread_mutex_lock(&timer_lock);
+/* Invalidates stale BBMD foreign device table entries */
+bacnet_bvlc_maintenance_timer(1);
+/* Transaction state machine: Responsible for retransmissions and ack
+* checking for confirmed services */
+bacnet_tsm_timer_milliseconds(1000);
+/* Re-enables communications after DCC_Time_Duration_Seconds
+* Required for SERVICE_CONFIRMED_DEVICE_COMMUNICATION_CONTROL
+* bacnet_dcc_timer_seconds(1); */
+/* State machine for load control object
+* Required for OBJECT_LOAD_CONTROL
+* bacnet_Load_Control_State_Machine_Handler(); */
+/* Expires any COV subscribers that have finite lifetimes
+* Required for SERVICE_CONFIRMED_SUBSCRIBE_COV
+* bacnet_handler_cov_timer_seconds(1); */
+/* Monitor Trend Log uLogIntervals and fetch properties
+* Required for OBJECT_TRENDLOG
+* bacnet_trend_log_timer(1); */
+/* Run [Object_Type]_Intrinsic_Reporting() for all objects in device
+* Required for INTRINSIC_REPORTING
+* bacnet_Device_local_reporting(); */
+/* Sleep for 1 second */
+pthread_mutex_unlock(&timer_lock);
+sleep(1);
 }
-
+return arg;
+}
 static void ms_tick(void) {
-    /* Updates change of value COV subscribers.
-     * Required for SERVICE_CONFIRMED_SUBSCRIBE_COV
-     * bacnet_handler_cov_task(); */
+/* Updates change of value COV subscribers.
+* Required for SERVICE_CONFIRMED_SUBSCRIBE_COV
+* bacnet_handler_cov_task(); */
 }
-
 #define BN_UNC(service, handler) \
-    bacnet_apdu_set_unconfirmed_handler(		\
-		    SERVICE_UNCONFIRMED_##service,	\
-		    bacnet_handler_##handler)
+bacnet_apdu_set_unconfirmed_handler( \
+SERVICE_UNCONFIRMED_##service, \
+bacnet_handler_##handler)
 #define BN_CON(service, handler) \
-    bacnet_apdu_set_confirmed_handler(			\
-		    SERVICE_CONFIRMED_##service,	\
-		    bacnet_handler_##handler)
+bacnet_apdu_set_confirmed_handler( \
+SERVICE_CONFIRMED_##service, \
+bacnet_handler_##handler)
+
+// modbus //
+static void *modbus(void *arg){
+	int i; /*variable*/
+	int ni; /*number of items modbus has received*/
+	uint16_t tabl[128]; /* storage for data from modbus request*/
+	char sending[64]; /*storage value for sending data*/
+	modbus_t *ctx; /*reference to modbus connection*/
+	confailed:;
+	ctx = modbus_new_tcp("140.159.153.159", 502); /*connects to server at VU, or at least prepares too */
+
+
+	if (ctx == NULL) {
+		fprintf(stderr, "Cannot locate libmodbus\n");
+		usleep(100000);/* waits 100 microsecond  */
+		goto confailed; /* tries again */
+	}
+	if (modbus_connect(ctx) == -1) {
+		printf("Connection failure");
+		fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+		modbus_free(ctx);
+		usleep(100000); /* waits 100 microsecond */
+		goto confailed; /* tries again */
+	}
+while (1) {
+	ni = modbus_read_registers(ctx, 40,2, tabl); /*reads the data from modbus server at reg 40 and reads 2 regs so 40 and 41 */
+
+	if (ni == -1) {
+		fprintf(stderr, "%s\n", modbus_strerror(errno));
+		modbus_close(ctx);
+		modbus_free(ctx);
+		usleep(100000); /* waits 100 microsecond */
+		goto confailed; /* tries again */
+	}
+
+	for (i=0; i < ni; i++) {
+		sprintf(sending, "%x", tabl[i]);
+		add_to_list(&list_head[i], sending);
+	}
+	usleep(100000); /* waits 100 microsecond before reading again */
+	}
+	return arg; /*removes warning*/
+}
 
 int main(int argc, char **argv) {
 uint8_t rx_buf[bacnet_MAX_MPDU];
